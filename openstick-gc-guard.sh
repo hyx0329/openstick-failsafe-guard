76467fbfd04d2950e6140c177cb27f79e67963e1
hyx0329 <hyx0329@163.com>
2022-07-30 11:39:20 +0800

    reorganize code
    Signed-off-by: hyx0329 <hyx0329@163.com>

--- openstick-gc-guard.sh
+++ openstick-gc-guard.sh
@@ -10,65 +10,74 @@ FAILSAFE_AP_PASSWORD="12345678"
 FAILSAFE_AP_CHANNEL=3
 FAILSAFE_AP_ADDRESS="192.168.69.1/24"
 
-test_usb_gadget_disconnected() {
-  # return normal(0) if in gadget mode and port is suspend(disconnected)
-  CMP_VALUE=$(awk '/^PORTSC.*/{ a = strtonum("0x" $3); exit } END { print and(a, 0x880) }' /sys/kernel/debug/usb/ci_hdrc.0/registers)
-  STATUS=$(cat /sys/kernel/debug/usb/ci_hdrc.0/role)
-  if [ 2176 -eq "$CMP_VALUE" ] && [ "gadget" = "$STATUS" ] ; then
-    return 0
-  fi
-  return 1
+get_usb_role() {
+  cat /sys/kernel/debug/usb/ci_hdrc.0/role
 }
 
-test_usb_host_connected() {
+is_gadeget_mode() {
+  [ "gadget" = $(get_usb_role) ]
+}
+
+is_host_mode() {
+  [ "host" = $(get_usb_role) ]
+}
+
+is_usb_connected_host_mode() {
+  # get status from usb registers, return normal(0) if connected
+  # following EHCI standard
   CMP_VALUE=$(awk '/^PORTSC.*/{ a = strtonum("0x" $3); exit } END { b = and(a, 0x800); c = and(a, 0x80); if (or(b,c) == b) { print b } else { print 0 }  }' /sys/kernel/debug/usb/ci_hdrc.0/registers)
-  STATUS=$(cat /sys/kernel/debug/usb/ci_hdrc.0/role)
-  if [ 0 -ne "$CMP_VALUE" ] && [ "host" = "$STATUS" ] ; then
-    return 0
-  fi
-  return 1
-}
-
-test_usb_connection_status() {
-  # if return 0(no error), then the usb is CONNECTED as a HOST
-
-  # WARNING: Requires gawk! must be explictly installed!
-  if awk -W version | grep -i -- GNU Awk ; then
-    CMP_VALUE=$(awk '/^PORTSC.*/{ a = strtonum("0x" $3); exit } END { print and(a, 0x80) }' /sys/kernel/debug/usb/ci_hdrc.0/registers)
-    if [ 0 -eq "$CMP_VALUE" ] ; then
-      return 1  # not connected
-    else
-      return 0  # connected
-    fi
+  if [ 0 -eq "$CMP_VALUE" ] ; then
+    return 1  # not connected
   else
-    logger "Warning: Please install gawk explictly to make the detection correct!"
-    return 1  # awk not supported, default not connected
+    return 0  # in host mode, the device is connected if PE is ONE and SUSP is ZERO
   fi
 }
 
-test_required_connection_nmcli() {
-  # if no error, the device is online
+is_usb_connected_gadget_mode() {
+  # return normal(0) if usb is connected, assuming it's gadget mode
+  CMP_VALUE=$(awk '/^PORTSC.*/{ a = strtonum("0x" $3); exit } END { print and(a, 0x80); }' /sys/kernel/debug/usb/ci_hdrc.0/registers)
+  if [ 0 -eq "$CMP_VALUE" ] ; then
+    return 0  # in gadget mode, the device is connected if SUSP bit is ZERO
+  else
+    return 1
+  fi
+}
 
-  if nmcli device | awk '{print $2, $3}' | grep -E -- "wifi" | grep " connected" > /dev/null ; then
-    return 0  # has wifi(AP/station) or ethernet(usb)
-  elif nmcli device | awk '{print $2, $3}' | grep -E -- "ethernet" | grep " connected" > /dev/null ; then
-    # we need to test if usb is connected
-    if test_usb_connection_status ; then
-      return 0  # gadget mode and connected
-    else
-      return 1  # gadget mode but disconnected
-    fi
+is_wifi_connected() {
+  nmcli device | awk '{print $2, $3}' | grep -E -- "wifi" | grep " connected" > /dev/null
+}
+
+is_ethernet_connected() {
+  # exclude usb interfaces
+  nmcli device | awk '{print $1, $2, $3}' | grep -E -- "ethernet" | grep -i -v -- "usb" | grep " connected" > /dev/null
+}
+
+is_usb_net_connected() {
+  nmcli device | awk '{print $1, $2, $3}' | grep -E -- "usb" | grep " connected" > /dev/null
+}
+
+is_device_online() {
+  # if no error, the device is online
+  if is_wifi_connected || is_ethernet_connected ; then
+    return 0  # has wifi(AP/station), or USB ethernet card connected (NOT RNDIS gadget mode)
+  elif is_usb_net_connected && is_gadeget_mode && is_usb_connected_gadget_mode ; then
+    return 0  # gadget mode and connected
   else
     return 1  # well, offline, disconnected, whatever
   fi
 }
 
 setup_gadget_rndis_network() {
+  # Setup the usb gadget internet interface if device is not
+  # connected to any USB gadgets as a host.
+
   # Test if host mode and connected to other gadget device.
-  # If so, abort.
+  # If so, abort. So connected usb devices will still work.
+  #
   # This won't block if device in host mode and connected
-  # to a host device.
-  if test_usb_host_connected ; then
+  # to a host device. And under this situation, the device
+  # is set to gadget mode automatically.
+  if is_host_mode && is_usb_connected_host_mode ; then
     return 1
   fi
 
@@ -87,11 +96,10 @@ setup_gadget_rndis_network() {
 }
 
 setup_failsafe_ap() {
-  # have to manually activate the network
-  # use a fail-safe hotspot
+  # Setup a failsafe AP with known password for recovery
 
   ### setup a failsafe ap ###
-  # have to use a password or NM just fails TvT
+  # it seems a password is required or NM just fails TvT
 
   # delete old connection anyway
   nmcli connection delete $FAILSAFE_AP_CON > /dev/null
@@ -119,18 +127,16 @@ setup_serial_ttyMSM0() {
 }
 
 main() {
-  if test_required_connection_nmcli ; then
-    logger "Device online, no action."
+  if is_device_online ; then
+    logger "Device online, good to go."
     return 0  # device online
   fi
   
   # oops, device offline, try manual activation
   logger "Device offline, trying to activate USB RNDIS interface."
-  if setup_gadget_rndis_network ; then
-    if test_required_connection_nmcli ; then
-      logger "Device back online."
-      return 0
-    fi
+  if setup_gadget_rndis_network && is_device_online ; then
+    logger "Device back online now. Lucky!"
+    return 0
   fi
 
   # oops, all failed
